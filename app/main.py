5bb46a88cac4975f16d2ced9f308646d76649deb
trgiangdo <dtr.giang.1299@gmail.com>
2026-01-07 18:16:44 +0700

    feature: create a REPL

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
