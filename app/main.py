a4f655c5025193bb740c8322c6895c0362bf19c8
trgiangdo <dtr.giang.1299@gmail.com>
2026-01-07 21:44:02 +0700

    feature: ability to execute program with args

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import subprocess
 import sys
 import os
 from typing import Callable, Dict
+
+
+def find_executable(command: str) -> str:
+    PATH = os.environ["PATH"]
+    for path in PATH.split(os.pathsep):
+        if not os.path.exists(path):
+            continue
+
+        file_path = os.path.join(path, command)
+        if os.access(file_path, os.X_OK):
+            return file_path
+
+    return ""
+
 
 def handle_exit(command: str):
     exit()
@@ -13,29 +28,21 @@ def handle_echo(command: str):
 
 def handle_type(command: str):
     message = command[5:]
-    builtin_command_strip = [command.strip() for command in builtin_command.keys()]
-    if message.strip() in builtin_command_strip:
+    if message.strip() in builtin_command:
         print(f"{message} is a shell builtin")
         return
 
-    PATH = os.environ["PATH"]
-    for path in PATH.split(os.pathsep):
-        if not os.path.exists(path):
-            continue
-
-        for filename in os.listdir(path):
-            file_path = os.path.join(path, filename)
-            if message == filename and os.path.isfile(file_path) and os.access(file_path, os.X_OK):
-                print((f"{message} is {file_path}"))
-                return
+    if exec_path := find_executable(message):
+        print((f"{message} is {exec_path}"))
+        return
 
     print(f"{message}: not found")
 
 
 builtin_command: Dict[str, Callable] = {
     "exit": handle_exit,
-    "echo ": handle_echo,
-    "type ": handle_type,
+    "echo": handle_echo,
+    "type": handle_type,
 }
 
 
@@ -43,13 +50,21 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_command = input()
+        args = user_command.split(" ")
 
         for command, command_handler in builtin_command.items():
-            if user_command.startswith(command):
+            if args[0] == command:
                 command_handler(user_command)
                 break
+            elif find_executable(args[0]):
+                result = subprocess.run(args, capture_output=True, text=True)
+                if result.stdout:
+                    print(result.stdout.strip())
+                if result.stderr:
+                    print(result.stderr.strip())
+                break
         else:
-            print(f"{user_command}: command not found")
+            print(f"{args[0]}: command not found")
 
 
 if __name__ == "__main__":
