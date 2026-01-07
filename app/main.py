25404889259ef037197d4371f5bf25dabe71decb
trgiangdo <dtr.giang.1299@gmail.com>
2026-01-07 20:16:01 +0700

    feature: implement type command with proper refactoring the handlers

--- app/main.py
+++ app/main.py
@@ -1,19 +1,42 @@
 import sys
+from typing import Callable, Dict
+
+def handle_exit(command: str):
+    exit()
+
+
+def handle_echo(command: str):
+    message = command[5:]
+    print(message)
+
+
+def handle_type(command: str):
+    message = command[5:]
+    builtin_command_strip = [command.strip() for command in builtin_command.keys()]
+    if message.strip() in builtin_command_strip:
+        print(f"{message} is a shell builtin")
+    else:
+        print(f"{message}: not found")
+
+
+builtin_command: Dict[str, Callable] = {
+    "exit": handle_exit,
+    "echo ": handle_echo,
+    "type ": handle_type,
+}
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_command = input()
 
-        if command == "exit":
-            exit()
-        if command.startswith("echo "):
-            message = command[5:]
-            print(message)
-            continue
-
-        print(f"{command}: command not found")
+        for command, command_handler in builtin_command.items():
+            if user_command.startswith(command):
+                command_handler(user_command)
+                break
+        else:
+            print(f"{user_command}: command not found")
 
 
 if __name__ == "__main__":
