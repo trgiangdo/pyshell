abea4dbce0ae408f53fe5ceccc2546d22f3d7acb
trgiangdo <dtr.giang.1299@gmail.com>
2026-01-07 18:10:24 +0700

    feature: initial shell setup

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
+    sys.stdout.write("$ ")
 
 
 if __name__ == "__main__":
